008367f3d056ccce9e47a381dcf98d94fe285323
2Hasan2 <133760904+2Hasan2@users.noreply.github.com>
2023-12-14 02:03:11 +0200

    `config_num.c`

---
+++ config_num.c
@@ -0,0 +1,43 @@
+#include "shell.h"
+/**
+ * is_digit - check if a character is a number.
+ * @c: is the character.
+ * Return: 1 digit or 0 otherwise.
+ */
+int is_digit(char c)
+{
+	if (c > 47 && c < 58)
+		return (1);
+	return (0);
+}
+/**
+ * convert_to_integer - convert a string to an integer.
+ * @s: pointer.
+ * Return: result entero.
+ */
+int convert_to_integer(char *s)
+{
+	int sign = 1;
+	unsigned int total = 0;
+	char working = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+			sign = sign * -1;
+		if (*s >= '0' && *s <= '9')
+		{
+			working = 1;
+			total = total * 10 + *s - '0';
+		}
+		else if (*s < '0' || *s > '9')
+		{
+			if (working)
+				break;
+		}
+		s++;
+	}
+	if (sign < 0)
+		total = (-(total));
+	return (total);
+}
